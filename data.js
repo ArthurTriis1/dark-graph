7f78fb7f21908953f850c092de42aa7e754c6fba
aluno <arthurfelandrade@gmail.com>
2020-07-19 00:21:19 -0300

    First commit

---
+++ data.js
@@ -0,0 +1,339 @@
+let g = {
+    nodes: [
+      {
+        id: "jonas",
+        label: "Jonas Kahnwald (Adam)",
+        url: "img/jonas.jpg",
+      },
+      {
+        id: "martha2",
+        label: "Martha Nilsen ",
+        url: "img/martha.jpeg",
+      },
+      {
+        id: "infinito",
+        label: "Trio do Infinito",
+        url: "img/infinito.jpeg",
+      },
+      {
+        id: "agnes",
+        label: "Agnes Nilsen",
+        url: "img/agnes.jpg",
+      },
+      {
+        id: "tronte",
+        label: "Tronte Nilsen",
+        url: "img/tronte.jpg",
+      },
+      {
+        id: "jana",
+        label: "Jana",
+        url: "img/jana.png"
+      },
+      {
+        id: "mads",
+        label: "Mads Nilsen",
+        url: "img/mads.jpg"
+      },
+      {
+        id: "ulrich",
+        label: "Ulrich Nilsen",
+        url: "img/ulrich1.jpg"
+      },
+      {
+        id: "katharina",
+        label: "Katharina Albers",
+        url: "img/katharina.jpg"
+      },
+      {
+        id: "hannah",
+        label: "Hannah Kahnwald",
+        url: "img/hannah.jpg"
+      },
+      {
+        id: "mikkel",
+        label: "Mikkel Nilsen / Michael Kahnwald",
+        url: "img/mikkel.jpg"
+      },
+      {
+        id: "magnus",
+        label: "Magnus Nilsen",
+        url: "img/magnus.png"
+      },
+      {
+        id: "martha1",
+        label: "Martha Nilsen",
+        url: "img/martha1.jpg"
+      },
+      {
+        id: "egon",
+        label: "Egon Tiedemann",
+        url: "img/egon.jpg"
+      },
+      {
+        id: "bartosz",
+        label: "Bartosz Tiedemann",
+        url: "img/bartosz.jpg"
+      },
+      {
+        id: "silja",
+        label: "Silja Tiedemann",
+        url: "img/silja.jpg"
+      },
+      {
+        id: "helene",
+        label: "Helene Albers",
+        url: "img/helene.png"
+
+      },
+      {
+        id: "hermann",
+        label: "Hermann Albers",
+        url: "img/hermann.jpg"
+      },
+      {
+        id: "noah",
+        label: "Noah/Hanno Tauber",
+        url: "img/noah.jpg"
+      },
+      {
+        id: "elisabeth",
+        label: "Elisabeth Doppler",
+        url: "img/elisabeth.jpg"
+
+      },
+      {
+        id: "charlotte",
+        label: "Charlotte Doppler",
+        url: "img/charlotte.jpg"
+      },
+      {
+        id: "hg",
+        label: "H.G. Tannhaus",
+        url: "img/hg.jpg"
+      },
+      {
+        id: "peter",
+        label: "Peter Doppler",
+        url: "img/peter.jpg"
+
+      },
+      {
+        id: "franziska",
+        label: "Franziska Doppler",
+        url: "img/franziska.png"
+
+      },
+      {
+        id: "greta",
+        label: "Greta Doppler",
+        url: "img/greta.jpg"
+
+      },
+      {
+        id: "helge",
+        label: "Helge Doppler",
+        url: "img/helge.jpg"
+      },
+      {
+        id: "claudia",
+        label: "Claudia Tiedemann",
+        url: "img/claudia.jpg"
+      },
+      {
+        id: "doris",
+        label: "Doris",
+        url: "img/doris.jpg"
+      },
+      {
+        id: "gretchen",
+        label: "Gretchen Tiedemann",
+        url: "img/gretchen.jpg"
+      },
+      {
+        id: "bernd",
+        label: "Bernd Doppler",
+        url: "img/bernd.jpg"
+      },
+      {
+        id: "regina",
+        label: "Regina Tiedemann",
+        url: "img/regina.jpg"
+      },
+      {
+        id: "boris",
+        label: "Boris/Aleksander",
+        url: "img/boris.jpg"
+      }
+
+    ],
+    edges: [
+      {
+        source: "regina",
+        target: "bartosz"
+      },
+      {
+        source: "boris",
+        target: "bartosz"
+      },
+      {
+        source: "bernd",
+        target: "regina"
+      },
+      {
+        source: "claudia",
+        target: "regina"
+      },
+      {
+        source: "claudia",
+        target: "gretchen",
+        type: "dotted"
+      },
+      {
+        source: "doris",
+        target: "claudia"
+      },
+      {
+        source: "egon",
+        target: "claudia"
+      },
+      {
+        source: "helge",
+        target: "peter"
+      },
+      {
+        source: "greta",
+        target: "helge"
+      },
+
+      {
+        source: "jonas",
+        target: "infinito",
+      },
+      {
+        source: "martha2",
+        target: "infinito",
+      },
+      {
+        source: "agnes",
+        target: "tronte",
+      },
+      {
+        source: "infinito",
+        target: "tronte"
+      },
+      {
+        source: "tronte",
+        target: "mads"
+      },
+      {
+        source: "jana",
+        target: "mads"
+      },
+      {
+        source: "tronte",
+        target: "ulrich"
+      },
+      {
+        source: "jana",
+        target: "ulrich"
+      },
+      {
+        source: "ulrich",
+        target: "mikkel"
+      },
+      {
+        source: "ulrich",
+        target: "magnus"
+      },
+      {        
+        source: "ulrich",
+        target: "martha1"
+      },
+      {        
+        source: "katharina",
+        target: "mikkel"
+      },
+      {        
+        source: "katharina",
+        target: "magnus"
+      },
+      {        
+        source: "katharina",
+        target: "martha1"
+      },
+      {        
+        source: "mikkel",
+        target: "jonas"
+      },
+      {        
+        source: "hannah",
+        target: "jonas",
+      },
+      {        
+        source: "egon",
+        target: "silja"
+      },
+      {        
+        source: "hannah",
+        target: "silja"
+      },
+      {        
+        source: "bartosz",
+        target: "agnes"
+      },
+      {
+        source: "silja",
+        target: "agnes"
+      },
+      {        
+        source: "bartosz",
+        target: "noah"
+      },
+      {
+        source: "silja",
+        target: "noah"
+      },
+      {
+        source: "helene",
+        target: "katharina"
+      },
+      {
+        source: "hermann",
+        target: "katharina"
+      },
+      {
+        source: "noah",
+        target: "charlotte"
+      },
+      {
+        source: "elisabeth",
+        target: "charlotte"
+      },
+      {
+        source: "hg",
+        target: "charlotte",
+        type: "dotted"
+      },
+      {
+        source: "peter",
+        target: "franziska"
+      },
+      {
+        source: "peter",
+        target: "elisabeth"
+      },
+      {
+        source: "charlotte",
+        target: "franziska"
+      },
+      {
+        source: "charlotte",
+        target: "elisabeth"
+      },
+      
+    
+    
+    
+    ]
+  }
---
+++ exec.js
@@ -0,0 +1,371 @@
+let loaded = 0,
+    L = 8,
+    N = g.nodes.length,
+    s,
+    clicked = false,
+    animationStep = 0;
+    state = 0,
+    labelsButton = ["Estabilizar Partícula", "Analisar", "Árvore Genelógica", "Oscilar"],
+    selectedId = "";
+
+const urls = g.nodes.filter(node => node.url).map(node => node.url);
+
+sigma.canvas.nodes.image = (function () {
+var _cache = {},
+    _loading = {},
+    _callbacks = {};
+
+// Return the renderer itself:
+var renderer = function (node, context, settings) {
+    var args = arguments,
+    prefix = settings("prefix") || "",
+    size = node[prefix + "size"],
+    color = node.color || settings("defaultNodeColor"),
+    url = node.url;
+
+    if (_cache[url]) {
+    context.save();
+
+    // Draw the clipping disc:
+    context.beginPath();
+    context.arc(
+        node[prefix + "x"],
+        node[prefix + "y"],
+        node[prefix + "size"],
+        0,
+        Math.PI * 2,
+        true
+    );
+    context.closePath();
+    context.clip();
+
+    // Draw the image
+    context.drawImage(
+        _cache[url],
+        node[prefix + "x"] - size,
+        node[prefix + "y"] - size,
+        2 * size,
+        2 * size
+    );
+
+    // Quit the "clipping mode":
+    context.restore();
+
+    // Draw the border:
+    context.beginPath();
+    context.arc(
+        node[prefix + "x"],
+        node[prefix + "y"],
+        node[prefix + "size"],
+        0,
+        Math.PI * 2,
+        true
+    );
+    context.lineWidth = size / 10;
+    context.strokeStyle = node.color || settings("defaultNodeColor");
+    context.stroke();
+    } else {
+    sigma.canvas.nodes.image.cache(url);
+    sigma.canvas.nodes.def.apply(sigma.canvas.nodes, args);
+    }
+};
+
+// Let's add a public method to cache images, to make it possible to
+// preload images before the initial rendering:
+renderer.cache = function (url, callback) {
+    if (callback) _callbacks[url] = callback;
+
+    if (_loading[url]) return;
+
+    var img = new Image();
+
+    img.onload = function () {
+    _loading[url] = false;
+    _cache[url] = img;
+
+    if (_callbacks[url]) {
+        _callbacks[url].call(this, img);
+        delete _callbacks[url];
+    }
+    };
+
+    _loading[url] = true;
+    img.src = url;
+};
+
+return renderer;
+})();
+
+function shuffle(array) {
+var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+// While there remain elements to shuffle...
+while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+}
+
+return array;
+}
+
+let animeGrafo = () => {  
+    let prefix = [
+        'random1_', 'random2_',
+        'random3_','random4_',
+        'random5_', 'random6_',
+        'random7_','random8_'
+    ][animationStep++];
+    sigma.plugins.animate(
+      s,
+      {
+        x: prefix + 'x',
+        y: prefix + 'y',
+        type: prefix + 'type',
+      }
+    );
+    animationStep = animationStep > 7 ? 0 : animationStep;
+}
+
+let intervalAnimeGrafo = setInterval(animeGrafo, 500);
+
+function inverter() {
+    clearInterval(intervalAnimeGrafo)
+    s.killForceAtlas2()
+    state++;
+
+    document.getElementById("btState").innerText = labelsButton[state]
+    if(state == 0){
+        
+        animeGrafo();
+        intervalAnimeGrafo = setInterval(animeGrafo, 500);
+        clicked = false;
+        s.graph.edges().forEach((ed) => (ed.color = "#000"));
+        s.refresh();
+
+    }
+    else if(state == 3){
+
+        s.startForceAtlas2();
+    }
+    else{
+        let prefix = ["circular_", "grid_"][state - 1];
+        sigma.plugins.animate(s, {
+            x: prefix + "x",
+            y: prefix + "y",
+            type: prefix + "type"
+        });
+    }
+
+    let container = document.getElementById("container");
+    setInterval(()=>{
+        if(!state == 0){
+            container.classList.remove("apagado");
+            container.classList.add("aceso")
+        }else{
+            container.classList.add("apagado");
+            container.classList.remove("aceso")
+        }
+    }, 1500)
+    
+    state = state >= 3 ? -1 : state
+    
+}
+
+
+
+g.nodes = shuffle(g.nodes).map((node, i) => {
+    return {
+        ...node,
+        circular_x: L * Math.cos((Math.PI * 2 * i) / N - Math.PI / 2),
+        circular_y: L * Math.sin((Math.PI * 2 * i) / N - Math.PI / 2),
+        grid_x: (i % L)/1.3,
+        grid_y: (Math.floor(i / L)),
+        random1_x: Math.random(),
+        random1_y: Math.random(),
+        random2_x: Math.random(),
+        random2_y: Math.random(),
+        random3_x: Math.random(),
+        random3_y: Math.random(),
+        random4_x: Math.random(),
+        random4_y: Math.random(),
+        random5_x: Math.random(),
+        random5_y: Math.random(),
+        random6_x: Math.random(),
+        random6_y: Math.random(),
+        random7_x: Math.random(),
+        random7_y: Math.random(),
+        random8_x: Math.random(),
+        random8_y: Math.random(),
+
+        //type style
+        random1_type: "default",
+        random2_type: "default",
+        random3_type: "default",
+        random4_type: "default",
+        random5_type: "default",
+        random6_type: "default",
+        random7_type: "default",
+        random8_type: "default",
+        circular_type: "image",
+        grid_type: "image",
+        x: Math.random(),
+        y: Math.random(),
+        color: "#000",
+        size: 1,
+        type: "default",
+    };
+});
+
+g.edges = g.edges.map((edge, index) => {
+return {
+    ...edge,
+    id: "e" + index,
+    type: edge.type ? edge.type : "curvedArrow",
+    color: edge.color ? edge.color : "#000",
+    size: 5,
+};
+});
+
+urls.forEach(function (url) {
+sigma.canvas.nodes.image.cache(url, function () {
+    if (++loaded === urls.length) {
+    s = new sigma({
+        graph: g,
+        renderer: {
+            container: document.getElementById("container"),
+            type: "canvas",
+        },
+        settings: {
+            sideMargin : 0.25,
+            defaultZoomingRatio: 2,
+            drawLabels: false,
+            defaultLabelColor: "#567893",
+            animationsTime: 500,
+            maxNodeSize: 28,
+            maxEdgeSize: 5,
+            minArrowSize: 5,
+            doubleClickEnabled: false,
+            enableEdgeHovering: false,
+            defaultHoverFont: 'Grenze Gotisch',
+            font: 'Grenze Gotisch',
+            defaultHoverLabelBGColor: "#000",
+            defaultLabelHoverColor: "#FFF",
+            singleHover: true,
+            labelSize: "proportional",
+            defaultLabelSize: "15",
+            labelThreshold: 16,
+            //enableCamera: false
+    },
+    });
+
+    s.bind("outNode", function (e) {
+        if(!state){
+            return;
+        }
+        if (!clicked) {
+        s.graph.edges().forEach((ed) => (ed.color = "#000"));
+        document.getElementById("container").style.cursor = "default";
+        s.refresh();
+        }
+    });
+
+    s.bind("doubleClickStage ", (e) => {
+        if(!state){
+            return;
+        }
+        clicked = false;
+        s.graph.edges().forEach((ed) => (ed.color = "#000"));
+        s.refresh();
+        // let container = document.getElementById("container");
+        // container.style.cursor = "default";
+        // container.classList.remove("showDetails")
+        //document.getElementById("details").classList.add("closeDetails")
+    });
+
+    s.bind("overNode", function (e) {
+        if(!state){
+            return;
+        }
+        if (!clicked) {
+        s.graph
+            .edges()
+            .forEach((ed) => (ed.color = "rgba(122, 150, 189, 0)"));
+        s.graph
+            .edges()
+            .filter((edge) => {
+            return edge.source === e.data.node.id;
+            })
+            .forEach((ed) => (ed.color = "#EFEDED"));
+
+        s.graph
+            .edges()
+            .filter((edge) => {
+            return edge.target === e.data.node.id;
+            })
+            .forEach((ed) => (ed.color = "rgb(8, 68, 117)"));
+
+        document.getElementById("container").style.cursor = "pointer";
+        s.refresh();
+        }
+    });
+
+    s.bind("clickNode", function (e) {
+        
+        if( e.data.node.id === selectedId){
+            if(!state){
+                return;
+            }
+            clicked = false;
+            s.graph.edges().forEach((ed) => (ed.color = "#000"));
+        }else{
+
+            if(!state){
+                return;
+            }
+            if (!clicked) {
+                clicked = true;
+            }
+            s.graph
+                .edges()
+                .forEach((ed) => (ed.color = "rgba(122, 150, 189, 0)"));
+            s.graph
+                .edges()
+                .filter((edge) => {
+                return edge.source === e.data.node.id;
+                })
+                .forEach((ed) => (ed.color = "#EFEDED"));
+            s.graph
+                .edges()
+                .filter((edge) => {
+                return edge.target === e.data.node.id;
+                })
+                .forEach((ed) => (ed.color = "rgb(8, 68, 117)"));
+        }
+
+        // let container = document.getElementById("container");
+        // container.style.cursor = "pointer";
+        // container.classList.add("showDetails");
+        // document.getElementById("details").classList.remove("closeDetails")
+        
+        // let i = 1
+        // let timer = setInterval(() => { 
+        //     s.refresh();
+        //     i++
+        //     i > 500 && clearInterval(timer);
+        // }, 1)
+        selectedId = e.data.node.id;
+        s.refresh();
+    });
+
+    
+    }
+});
+});
